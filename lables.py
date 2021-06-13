d4e44d77b6bd56fafc6849c39e98dd31199a3d87
Ritik Raj <hrr0077@gmail.com>
2021-06-13 10:19:56 +0530

    First Commit

---
+++ lables.py
@@ -0,0 +1,11 @@
+import time
+import requests
+
+print("This is a file which help you tackel down the issue of fake Ig Profiles")
+time.sleep(3)
+print("Wait, Server is Starting.....")
+time.sleep(4)
+print("Server is tideup to Rock.....")
+time.sleep(2)
+user_name = input("Enter the account username = ")
+print(user_name)
